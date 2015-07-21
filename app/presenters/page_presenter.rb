34de2912bac6c9e13d8a2645c6b87a05d5b9cb97
Optimised server <support@eskimosoup.co.uk>
2015-07-21 15:11:57 +0100

    Module links, contact form, pages

---
+++ app/presenters/page_presenter.rb
@@ -0,0 +1,15 @@
+class PagePresenter < BasePresenter
+  presents :page
+
+  def title
+    page.title
+  end
+
+  def content
+    h.raw page.content
+  end
+
+  def image
+    h.image_tag page.image.show, alt: page.title, class: 'page-image image-right' if page.image?
+  end
+end
