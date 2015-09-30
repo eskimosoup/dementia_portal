f54e6a3b1db5b3e0a9ffd0888a09ac4e3508a133
James Doyley <jdoyley@gmail.com>
2015-09-30 13:20:22 +0100

    Remove postcode from organisation

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -5,7 +5,6 @@ class Organisation < ActiveRecord::Base
   has_many :resources, dependent: :nullify
 
   validates :name, presence: true, uniqueness: true
-  validates :postcode, :organisation_type, presence: true
   validates :organisation_type, presence: true, inclusion: { in: ORGANISATION_TYPES }
 
 end
--- spec/models/organisation_spec.rb
+++ spec/models/organisation_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe Organisation, type: :model do
     subject { build(:organisation) }
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name) }
-    it { should validate_presence_of(:postcode) }
     it { should validate_presence_of(:organisation_type) }
     it { should validate_inclusion_of(:organisation_type).in_array(Organisation::ORGANISATION_TYPES) }
   end
